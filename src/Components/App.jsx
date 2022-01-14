2612aaa53c21642a5638b35c1aac86abcba0bc2d
Cyanne Ramnarine <lauralee.gookool@outlook.com>
2022-01-13 20:15:01 -0500

    created the input boxes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import Header from "./Header";
+import TextCompare from "./Pages/TextCompare";
 
 function App() {
   return (
     <div>
       <Header text="Text Comparison Tool"/>
+      <TextCompare/>
     </div>
   );
 }
